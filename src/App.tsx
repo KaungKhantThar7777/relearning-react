2632ed034ce6cda45a785d392dac42b00040bfec
kaungkhantthar7777 <kaungkhantthar77@gmail.com>
2023-04-27 21:08:40 +0630

    added: list group

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import ListGroup from "./components/ListGroup";
+
+type Props = {};
+
+const App = (props: Props) => {
+  return (
+    <div>
+      <ListGroup />
+    </div>
+  );
+};
+
+export default App;
